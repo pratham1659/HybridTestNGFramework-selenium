557719f06b9a75eb43501666377bf24db6d49524
prathamkumar <prathamkumar1985@gmail.com>
2024-03-08 02:29:50 +0530

    New Commit Added

---
+++ src/test/java/com/testNg/testcases/LoginPage.java
@@ -0,0 +1,17 @@
+package com.testNg.testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class LoginPage {
+
+	
+	@Test
+	public void verifyLoginWithValidCredentials () {
+		
+
+		WebDriver driver = new ChromeDriver();
+	}
+}
